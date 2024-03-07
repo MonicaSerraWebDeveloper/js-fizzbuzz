7884410f5802a88ae7557fd3486e71b5754d2022
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-07 15:46:45 +0100

    feat: add the string BuzzFizz to multiple of 3 and 5, string Fizz to multiple of 3 and string Buzz to multiple 5 stamp it in console

--- js/script.js
+++ js/script.js
@@ -6,10 +6,18 @@
 
 // Creiamo un ciclo che ci permetta di stampare in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
 
-// Andiamo a stabilire con if che i multipli di 3 gli venga assegnata la stringa "Fizz"
-// Andiamo a stabilire con else if che i multipli di 5 gli venga assegnata la stringa "Buzz"
-// Andiamo a stabilire che i multipli sia di 3 che di 5 gli venga assegnata la stringa "FizzBuzz"
+    let multipleNumber;
+    if (i % 3 === 0 && i % 5 === 0) { // Andiamo a stabilire con if che i multipli sia di 3 che di 5 gli venga assegnata la stringa "FizzBuzz"
+        multipleNumber = 'BuzzFizz';
+    }  else if (i % 3 === 0) { // Andiamo a stabilire con else if che i multipli di 3 gli venga assegnata la stringa "Fizz"
+        multipleNumber = 'Fizz';
+    } else if (i % 5 === 0) { // Andiamo a stabilire con else if che i multipli di 5 gli venga assegnata la stringa "Buzz"
+        multipleNumber = 'Buzz';
+    } else { // Per tutti numeri restanti andiamo a dargli il valore numerico corrispondente a i
+        multipleNumber = i; 
+    }      
+    console.log(multipleNumber);
+};
+
 
