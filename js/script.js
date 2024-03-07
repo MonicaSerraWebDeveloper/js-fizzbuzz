2694115c16b027949351c22f0b1a4e9ab9562144
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-07 14:16:26 +0100

    feat: add all the comment to understand the sequence of the workflow

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+// per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+// Creiamo un ciclo che ci permetta di stampare in console i numeri da 1 a 100
+// Andiamo a stabilire con if che i multipli di 3 gli venga assegnata la stringa "Fizz"
+// Andiamo a stabilire con else if che i multipli di 5 gli venga assegnata la stringa "Buzz"
+// Andiamo a stabilire che i multipli sia di 3 che di 5 gli venga assegnata la stringa "FizzBuzz"
+
