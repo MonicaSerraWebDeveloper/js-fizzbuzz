4bd72eebe081b9311a107654c35b6be974fa7ff7
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-07 14:19:11 +0100

    feat: add cicle for to count the number from 1 to 100

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@
 
 
 // Creiamo un ciclo che ci permetta di stampare in console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
 // Andiamo a stabilire con if che i multipli di 3 gli venga assegnata la stringa "Fizz"
 // Andiamo a stabilire con else if che i multipli di 5 gli venga assegnata la stringa "Buzz"
 // Andiamo a stabilire che i multipli sia di 3 che di 5 gli venga assegnata la stringa "FizzBuzz"
